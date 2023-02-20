004a4c8b7cd7950ccba374ed9399c27c530e2b77
subin606 <gaemilee606@gmail.com>
2023-02-21 02:03:02 +0900

    댓글 분석 후 결과 파일을 만들어라
    애플리케이션이 실행되면 먼저 댓글 목록과 학교 이름 목록 csv 파일을 읽습니다.
    그리고 댓글에 언급된 학교 이름을 카운트 합니다.
    카운트된 결과를 result.txt 파일에 작성 후 애플리케이션이 종료됩니다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import application.CommentsReader;
+import fileutils.CsvReader;
+import application.ResulFileWriter;
+import application.SchoolNameRepetitionCounter;
+import application.SchoolNamesReader;
+import fileutils.FileWriter;
+import org.apache.commons.csv.CSVFormat;
+import pojo.Comment;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        CsvReader csvReader = new CsvReader(CSVFormat.INFORMIX_UNLOAD);
+
+        CommentsReader commentsReader = new CommentsReader(csvReader);
+        List<Comment> comments = commentsReader.read();
+
+        SchoolNamesReader schoolNamesReader = new SchoolNamesReader(csvReader);
+        List<String> schools = schoolNamesReader.read();
+
+        SchoolNameRepetitionCounter counter = new SchoolNameRepetitionCounter();
+
+        ResulFileWriter resulFileWriter
+                = new ResulFileWriter(new FileWriter());
+        resulFileWriter.write(
+                counter.countSchoolNameRepetitions(comments, schools)
+        );
     }
 }
