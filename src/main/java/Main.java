5b8d105242c1d876860f67294aed6ca1cb9c803a
subin606 <gaemilee606@gmail.com>
2023-02-22 03:28:43 +0900

    잘못된 클래스명을 고쳐라

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import application.CommentPreparer;
 import application.CommentsReader;
 import fileutils.CsvReader;
-import application.ResulFileWriter;
+import application.ResultFileWriter;
 import application.SchoolNameRepetitionCounter;
 import application.SchoolNamesReader;
 import fileutils.FileWriter;
 import org.apache.commons.csv.CSVFormat;
+import org.tinylog.Logger;
 
 import java.util.List;
 
@@ -14,16 +15,20 @@ public class Main {
     public static void main(String[] args) {
         CsvReader csvReader = new CsvReader(CSVFormat.INFORMIX_UNLOAD);
 
+        Logger.info("댓글리스트 파일 읽기를 시작합니다.");
         CommentsReader commentsReader = new CommentsReader(csvReader);
         List<String> comments = commentsReader.read();
+        Logger.info("댓글리스트 파일 읽기를 완료했습니다.");
 
+        Logger.info("학교 이름 리스트 파일 읽기를 시작합니다.");
         SchoolNamesReader schoolNamesReader = new SchoolNamesReader(csvReader);
         List<String> schools = schoolNamesReader.read();
+        Logger.info("학교 이름 리스트 파일 읽기를 완료했습니다.");
 
         SchoolNameRepetitionCounter counter
                 = new SchoolNameRepetitionCounter(new CommentPreparer());
 
-        ResulFileWriter resulFileWriter = new ResulFileWriter(new FileWriter());
-        resulFileWriter.write(counter.countSchoolNameRepetitions(comments, schools));
+        ResultFileWriter resultFileWriter = new ResultFileWriter(new FileWriter());
+        resultFileWriter.write(counter.countSchoolNameRepetitions(comments, schools));
     }
 }
--- src/main/java/application/ResultFileWriter.java
+++ src/main/java/application/ResultFileWriter.java
@@ -1,26 +1,29 @@
 package application;
 
 import fileutils.FileWriter;
+import org.tinylog.Logger;
 
 import java.io.IOException;
 import java.util.Map;
 
-/** 학교별 카툰트 결과를 txt 파일에 작성합니다. */
-public class ResulFileWriter {
+/** 학교별 카운트 결과를 txt 파일에 작성합니다. */
+public class ResultFileWriter {
 
     private static final String FILE_NAME = "result.txt";
     private static final String FORMAT = "%s\t%d\n";
 
     private final FileWriter fileWriter;
 
-    public ResulFileWriter(final FileWriter fileWriter) {
+    public ResultFileWriter(final FileWriter fileWriter) {
         this.fileWriter = fileWriter;
     }
 
     /** 주어진 결과를 txt 파일에 작성합니다. */
     public void write(Map<String, Long> results) {
         try {
+            Logger.info("결과를 {} 파일에 저장합니다.", FILE_NAME);
             fileWriter.write(FILE_NAME, convertMapToString(results));
+            Logger.info("결과가 정상적으로 저장되었습니다.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -28,6 +31,7 @@ public class ResulFileWriter {
 
     /** 결과가 담긴 Map이 주어지면 결과 저장 형식에 맞는 문자열로 변환해 반환합니다. */
     private String convertMapToString(Map<String, Long> map) {
+        Logger.info("찾은 학교 갯수: {}", map.size());
         StringBuffer stringBuffer = new StringBuffer();
 
         map.forEach((key, value) ->
