1178aa5b9403465f82194b55f378e1a192141b30
subin606 <gaemilee606@gmail.com>
2023-03-07 15:00:19 +0900

    메인 클래스에서 로그출력 부분을 제거하라

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,24 +6,21 @@ import application.SchoolNameRepetitionCounter;
 import application.SchoolNamesReader;
 import fileutils.FileWriter;
 import org.apache.commons.csv.CSVFormat;
-import org.tinylog.Logger;
 
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        CsvReader csvReader = new CsvReader(CSVFormat.INFORMIX_UNLOAD);
-
-        Logger.info("댓글리스트 파일 읽기를 시작합니다.");
-        CommentsReader commentsReader = new CommentsReader(csvReader);
+        CommentsReader commentsReader = new CommentsReader(
+                new CsvReader(CSVFormat.INFORMIX_UNLOAD)
+        );
         List<String> comments = commentsReader.read();
-        Logger.info("댓글리스트 파일 읽기를 완료했습니다.");
 
-        Logger.info("학교 이름 리스트 파일 읽기를 시작합니다.");
-        SchoolNamesReader schoolNamesReader = new SchoolNamesReader(csvReader);
+        SchoolNamesReader schoolNamesReader = new SchoolNamesReader(
+                new CsvReader(CSVFormat.INFORMIX_UNLOAD)
+        );
         List<String> schools = schoolNamesReader.read();
-        Logger.info("학교 이름 리스트 파일 읽기를 완료했습니다.");
 
         SchoolNameRepetitionCounter counter
                 = new SchoolNameRepetitionCounter(new CommentPreparer());
